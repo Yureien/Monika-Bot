e5595f6b638b093bcd07332c2eec6b944932e1e5
Soham Sen <sohamsen1000@gmail.com>
2018-02-15 18:25:33 +0530

    Moved to my VPS

--- main.py
+++ main.py
@@ -44,8 +44,7 @@ if not isDevMode:
     updater.start_webhook(listen="0.0.0.0",
                           port=PORT,
                           url_path=TOKEN)
-    updater.bot.set_webhook("https://lilmonix3t.sohamsen.me/" + TOKEN,
-                            certificate=open('/srv/ssl/cloudflare/sohamsen.me.cf.pem', 'rb'))
+    updater.bot.set_webhook("https://lilmonix3t.sohamsen.me/" + TOKEN)
     updater.idle()
 else:
     if 'IS_TRAVIS' in os.environ:  # Terminate after 10s if Travis
